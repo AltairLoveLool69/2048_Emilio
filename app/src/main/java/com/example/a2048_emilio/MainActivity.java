4b0ddaf49a3a3cd0889a6ec270e144e051de9336
Cursos LABDESI <Cursos@T20210.ds.iteso.mx>
2019-07-09 14:41:34 -0500

    parte1 app

---
+++ app/src/main/java/com/example/a2048_emilio/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.a2048_emilio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        startBtn = findViewById(R.id.main_activity_startbtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.bounce);
+                        startBtn.startAnimation(animation);
+                Intent intent = new Intent(MainActivity.this, Activity_game.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
